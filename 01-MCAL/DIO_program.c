efec2cedd1bf3af5e201018944e5e3cc82e45834
Sarah <70871300+SarahAbdrabo@users.noreply.github.com>
2022-07-21 06:48:27 +0200

    Update DIO_program.c

--- 01-MCAL/DIO_program.c
+++ 01-MCAL/DIO_program.c
@@ -42,18 +42,18 @@ void  DIO_voidSetPinValue  (u8 Copy_u8PORT, u8 Copy_u8PIN, u8 Copy_u8Value ){
 	if ( (Copy_u8PORT <4)  && (Copy_u8PIN < 8)){
 		if (Copy_u8Value == HIGH){
 			switch(Copy_u8PORT){
-				case PORTA  :     SET_BIT(PORTA_REG, Copy_u8Value); break;
-				case PORTB  :     SET_BIT(PORTB_REG, Copy_u8Value); break;
-				case PORTC  :     SET_BIT(PORTC_REG, Copy_u8Value); break;
-				case PORTD  :     SET_BIT(PORTD_REG, Copy_u8Value); break;
+				case PORTA  :     SET_BIT(PORTA_REG, Copy_u8PIN); break;
+				case PORTB  :     SET_BIT(PORTB_REG, Copy_u8PIN); break;
+				case PORTC  :     SET_BIT(PORTC_REG, Copy_u8PIN); break;
+				case PORTD  :     SET_BIT(PORTD_REG, Copy_u8PIN); break;
 			}
 		}
 		else if (Copy_u8Value == LOW){ 
 			switch(Copy_u8PORT){
-				case PORTA   :    CLR_BIT(PORTA_REG, Copy_u8Value); break;
-				case PORTB   :    CLR_BIT(PORTB_REG, Copy_u8Value); break;
-				case PORTC   :    CLR_BIT(PORTC_REG, Copy_u8Value); break;
-				case PORTD   :    CLR_BIT(PORTD_REG, Copy_u8Value); break;
+				case PORTA   :    CLR_BIT(PORTA_REG, Copy_u8PIN); break;
+				case PORTB   :    CLR_BIT(PORTB_REG, Copy_u8PIN); break;
+				case PORTC   :    CLR_BIT(PORTC_REG, Copy_u8PIN); break;
+				case PORTD   :    CLR_BIT(PORTD_REG, Copy_u8PIN); break;
 			}
 		}
 		else {/*                 nothing                             */}
